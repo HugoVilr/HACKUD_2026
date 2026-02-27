425796fcbdcce7974827af5837b2be2ffcddb717
MateoPalmeiro <mateo.palmeiro@udc.es>
2026-02-27 22:53:11 +0100

    esstructura base

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+// Clave única para guardar el vault cifrado en el navegador
+export const VAULT_STORAGE_KEY = "hackudc.vault.encrypted.v1";
+
+// Por si luego queréis separar settings
+export const APP_NAME = "Seeking the Perfect Key";
